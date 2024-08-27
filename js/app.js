d0c472b9a150a8887022f75c368967215fb77bc4
jennie <mikjk0530@gmail.com>
2024-08-27 18:06:42 +0900

    Fix: 삭제 기능 수정

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //app.js
-import { saveStudent, readStudents, deleteStudent } from "./modules/storage.js";
+import { saveStudent, readStudents } from "./modules/storage.js";
 
 class Student {
   constructor(studentNum, name, kor, eng, math) {
@@ -13,16 +13,15 @@ class Student {
     this.rank = 0;
   }
 }
-// TODO 로컬 스토리지
-
 class StudentRepository {
   constructor() {
-    this.students = [];
+    this.students = readStudents();
   }
   addStudent(student) {
     this.students.push(student);
     this.sortRank();
     this.addRank();
+    saveStudent(this.students);
   }
   addRank() {
     this.students.forEach((el, idx) => {
@@ -34,10 +33,20 @@ class StudentRepository {
       return b.avg - a.avg;
     });
   }
+  deleteStudent(studentNum, studentName) {
+    if (studentNum) {
+      this.students = this.students.filter(
+        (el) => el.studentNum !== parseInt(studentNum),
+      );
+    } else if (studentName) {
+      this.students = this.students.filter((el) => el.name !== studentName);
+    }
+    saveStudent(this.students);
+  }
 }
 
 let studentRepository = new StudentRepository();
-let students = studentRepository.students;
+// let students = studentRepository.students; => 배열을 변수로 저장(복사)하면 원본 배열이 업데이트 돼도 복사본은 변하지 않음. 따라서 변수로 저장하면 X
 const addBtn = document.querySelector("#addBtn");
 const searchBtn = document.querySelector("#searchBtn");
 const deleteBtn = document.querySelector("#deleteBtn");
@@ -73,6 +82,8 @@ const sortSelect = document.querySelector("#sortSelect");
 const searchSelect = document.querySelector("#searchSelect");
 const searchInput = document.querySelector("#searchInput");
 
+tBody.innerHTML = displayForm(studentRepository.students); // 초기 학생 목록 표시
+
 addBtn.addEventListener("click", () => {
   const newStudent = new Student(
     parseInt(inputNum.value),
@@ -81,53 +92,60 @@ addBtn.addEventListener("click", () => {
     parseInt(inputEng.value),
     parseInt(inputMath.value),
   );
-  studentRepository.addStudent(newStudent);
   readStudents(); // 첨에 안읽어오면 버튼 첨에 클릭했을 때 학생이 추가되는 게 아니라 빈 배열이 스토리지에 저장됨
-  saveStudent(newStudent);
-  tBody.innerHTML = displayForm(students);
+  studentRepository.addStudent(newStudent);
+  tBody.innerHTML = displayForm(studentRepository.students);
+  inputNum.value = "";
+  inputName.value = "";
+  inputKor.value = "";
+  inputEng.value = "";
+  inputMath.value = "";
 });
 
 deleteBtn.addEventListener("click", () => {
-  students = readStudents();
-  let result;
-  if (inputNum.value) {
-    result = students.filter(
-      (el) => el.studentNum !== parseInt(inputNum.value),
-    );
-  } else if (inputName.value) {
-    result = students.filter((el) => el.name !== inputName.value);
-  }
-  deleteStudent(result);
-  students = result;
-  tBody.innerHTML = displayForm(students);
+  studentRepository.deleteStudent(inputNum.value, inputName.value);
+
+  tBody.innerHTML = displayForm(studentRepository.students);
   alert("삭제됐습니다");
+
+  inputNum.value = "";
+  inputName.value = "";
 });
 
 sortSelect.addEventListener("change", (e) => {
   if (sortSelect.value === "rank") {
     studentRepository.sortRank();
-    tBody.innerHTML = displayForm(students);
+    tBody.innerHTML = displayForm(studentRepository.students);
   } else if (sortSelect.value === "ssn") {
-    let result = students.sort((a, b) => {
+    let result = studentRepository.students.sort((a, b) => {
       return a.studentNum - b.studentNum;
     });
     tBody.innerHTML = displayForm(result);
   } else if (sortSelect.value === "name") {
-    let result = students.sort((a, b) => {
+    let result = studentRepository.students.sort((a, b) => {
       return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
     });
     tBody.innerHTML = displayForm(result);
   }
 });
 
-searchBtn.addEventListener("click", () => {
-  if (searchSelect.value === "ssn") {
-    let result = students.filter(
+searchBtn.addEventListener("click", (e) => {
+  console.log(e);
+  if (
+    (searchSelect.value === "ssn" || searchSelect.value === "name") &&
+    searchInput.value === ""
+  ) {
+    let result = readStudents();
+    tBody.innerHTML = displayForm(result);
+  } else if (searchSelect.value === "ssn") {
+    let result = studentRepository.students.filter(
       (el) => el.studentNum === parseInt(searchInput.value),
     );
     tBody.innerHTML = displayForm(result);
   } else if (searchSelect.value === "name") {
-    let result = students.filter((el) => el.name === searchInput.value);
+    let result = studentRepository.students.filter(
+      (el) => el.name === searchInput.value,
+    );
     //find 사용하면 결과가 요소 1개, filter 사용하면 만족하는 요소 모두 새로운 배열로 반환.
     tBody.innerHTML = displayForm(result);
   }
--- js/modules/storage.js
+++ js/modules/storage.js
@@ -1,25 +1,15 @@
-const saveStudent = function (student) {
-  let students = readStudents(); // 항상 최신 데이터를 읽어옴
-  students.push(student); // 새 학생 추가
+const saveStudent = function (students) {
   const json = JSON.stringify(students);
   localStorage.setItem("students", json); // 업데이트된 배열을 저장
 };
 
-const deleteStudent = function (filteredStudents) {
-  // filteredStudents는 이미 삭제된 학생을 제외한 배열임.
-  const json = JSON.stringify(filteredStudents);
-  localStorage.setItem("students", json);
-};
-
 const readStudents = function () {
-  let students;
+  let students = [];
 
   if (localStorage.getItem("students")) {
     students = JSON.parse(localStorage.getItem("students"));
-  } else {
-    students = [];
   }
   return students;
 };
 
-export { saveStudent, deleteStudent, readStudents };
+export { saveStudent, readStudents };
